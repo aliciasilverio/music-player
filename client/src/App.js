1c1dd54b51c87417a1906775724edc9dec1e78c8
Alicia Silverio <aliciaalicia2455@gmail.com>
2022-07-08 08:17:01 -0400

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Login';
+import Dashboard from './Dashboard';
+
+// get the url param called code
+const code = new URLSearchParams(window.location.search).get('code');
 
 function App() {
-  return <Login />
+  // if we have a code, reder out dashboard and pass in, otherwise render login component
+  return code ? <Dashboard code={code} /> : <Login />
 }
 
 export default App;
