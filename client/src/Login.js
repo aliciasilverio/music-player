e45cc8835efb7579a167d2e0a83ed9ab68db302e
Alicia Silverio <aliciaalicia2455@gmail.com>
2022-07-08 08:06:23 -0400

    server setup, tokens

--- client/src/Login.js
+++ client/src/Login.js
@@ -6,10 +6,9 @@ const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=ad83f1d743b14
 
 export default function Login() {
     return (
-        <div>
-            <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh"}}>
-                <a className="btn btn-success btn-lg" href={AUTH_URL}>Login with Spotify</a>
-            </Container>
-        </div>
+        <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: "100vh"}}>
+            <a className="btn btn-success btn-lg" href={AUTH_URL}>Login with Spotify
+            </a>
+        </Container>
     )
 }
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.post('login', (req, res) => {
+    const code = request.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: 'ad83f1d743b14e8c95147919a61b8c4f',
+        clientSecret: '5e4ad69013e844af9c71d0fd9f4af1fd',
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refresh_token: data.body.refresh_token,
+            expires_in: data.body.expires_in
+
+        })
+    }) .catch(() => {
+        res.sendStatus(400);
+})
+        
+})
